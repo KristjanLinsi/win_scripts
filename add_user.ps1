32120c8e43ed6572dea03b80bf72d4876b16fbf2
Kristja Linsi <kristjan.linsi@khk.ee>
2022-03-28 09:17:30 +0300

    kasutajate lisamine powershelliga

---
+++ add_user.ps1
@@ -0,0 +1,3 @@
+﻿# add local user with name Kasutaja2
+$userPassword = ConvertTo-SecureString "Parool1!" -AsPlainText -Force
+New-LocalUser "Kasutaja2" -Password $userPassword -FullName "Esimmene Kasutaja" -Description "Local Accont -kasutaja2"
